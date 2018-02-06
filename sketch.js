02a74a5243799992f73ab4993c5b3bacfa0bdceb
NataliaLaverde <35973387+NataliaLaverde@users.noreply.github.com>
2018-02-05 23:14:47 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,145 @@
+var posX = 70;
+var posY = 120;
+var X = 10;
+var Y = 0;
+var X2 = 0;
+var Y2 = 60;
+
+function setup() { 
+  createCanvas(440, 240);
+
+  background(249, 255, 252);
+	fill (0,0,0)
+	rect (10, 120, 10, 120);
+	rect (30, 120, 10, 120);
+	rect (50, 120, 10, 120);
+	rect (0, 130, 440, 10);
+	rect (0, 150, 440, 10);
+	rect (0, 170, 440, 10);
+		for (var numFila = 0; numFila < 3; numFila = numFila+1) {
+		  strokeWeight (0); 
+			fill(250, 80, 75);
+			rect(posX, posY, 10, 120);
+	
+			posX = posX + 20;
+		}
+	for (var numFila = 0; numFila < 3; numFila = numFila+1) {
+		  strokeWeight (0); 
+			fill(255, 225, 163);
+			rect(posX, posY, 10, 120);
+	
+			posX = posX + 20;
+		}
+		for (var numFila = 0; numFila < 3; numFila = numFila+1) {
+		  strokeWeight (0); 
+			fill(255, 255, 152);
+			rect(posX, posY, 10, 120);
+	
+			posX = posX + 20;
+		}
+	
+	
+
+	for (var numFila = 0; numFila < 3; numFila = numFila+1) {
+		  strokeWeight (0); 
+			fill(230, 254, 255);
+			rect(posX, posY, 10, 120);
+	
+			posX = posX + 20;
+		}
+	for (var numFila = 0; numFila < 3; numFila = numFila+1) {
+		  strokeWeight (0); 
+			fill(255, 254, 255);
+			rect(posX, posY, 10, 120);
+	
+			posX = posX + 20;
+		}
+	
+	for (var numFila = 0; numFila < 3; numFila = numFila+1) {
+		  strokeWeight (0); 
+			fill(255, 227, 226);
+			rect(posX, posY, 10, 120);
+	
+			posX = posX + 20;
+		}
+	
+	
+	for (var numFila = 0; numFila < 3; numFila = numFila+1) {
+		  strokeWeight (0); 
+			fill(255, 254, 255);
+			rect(posX, posY, 10, 120);
+	
+			posX = posX + 20;
+		}
+	fill (250,80,75);
+	rect (430, 120, 10, 120);
+	fill (0,0,0);
+	rect (0, 210, 440, 10);
+	rect (0, 230, 440, 10);
+	rect (0, 190, 440, 10);
+	
+		for (var numFila = 0; numFila < 3; numFila = numFila+1) {
+		  strokeWeight (0); 
+			fill (255,55,53);
+			rect(X, Y, 10, 120);
+	
+			X = X + 20;
+		}
+for (var numFila = 0; numFila < 3; numFila = numFila+1) {
+		  strokeWeight (0); 
+			fill(255, 225, 163);
+			rect(X, Y, 10, 120);
+	
+			X = X + 20;
+		}
+
+	for (var numFila = 0; numFila < 3; numFila = numFila+1) {
+		  strokeWeight (0); 
+			fill(255, 254, 255);
+			rect(X, Y, 10, 120);
+	
+			X = X + 20;
+		}
+for (var numFila = 0; numFila < 3; numFila = numFila+1) {
+		  strokeWeight (0); 
+			fill(255, 227, 226);
+			rect(X, Y, 10, 120);
+	
+			X = X + 20;
+		}
+	
+	for (var numFila = 0; numFila < 3; numFila = numFila+1) {
+		  strokeWeight (0); 
+			fill (250,80,75);
+			rect(X, Y, 10, 120);
+	
+			X = X + 20;
+		}
+	for (var numFila = 0; numFila < 3; numFila = numFila+1) {
+		  strokeWeight (0); 
+			fill(255, 255, 152);
+			rect(X, Y, 10, 120);
+	
+			X = X + 20;
+		}
+		for (var numFila = 0; numFila < 3; numFila = numFila+1) {
+		  strokeWeight (0); 
+			fill (250,80,75);
+			rect(X, Y, 10, 120);
+	
+			X = X + 20;
+		}
+	
+	fill(255, 225, 163);
+	rect (430, 0, 10, 120);
+	for (var numFila = 0; numFila < 22; numFila = numFila+1) {
+		  strokeWeight (0); 
+			fill (0,0,0);
+			rect(X2, Y2, 10, 60);
+	
+			X2 = X2 + 20;
+		}
+	
+	
+	
+}
